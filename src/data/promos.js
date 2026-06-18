5d0a6e0342b0cd0048e31d2e12b76622be87fde0
Maximo <maximoferdman@gmail.com>
2026-06-18 17:01:17 -0300

    actualizo

---
+++ src/data/promos.js
@@ -0,0 +1,72 @@
+export const promos = [
+  {
+    id: 1,
+    name: "Promo 1",
+    emoji: "🍦",
+    price: 38700,
+    items: [
+      {
+        id: "agua",
+        name: "Agua 2 sabores x40",
+        variants: null
+      },
+      {
+        id: "crema",
+        name: "Palito de crema x40",
+        variants: [
+          "Dulce de leche y frutilla",
+          "Chocolate y vainilla"
+        ]
+      },
+      {
+        id: "bombon",
+        name: "Palito bombón americana x40",
+        variants: null
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: "Promo 2",
+    emoji: "🎁",
+    price: 65000,
+    items: [
+      {
+        id: "agua",
+        name: "Agua 2 sabores x40",
+        variants: null
+      },
+      {
+        id: "crema",
+        name: "Palito de crema x40",
+        variants: [
+          "Dulce de leche y frutilla",
+          "Chocolate y vainilla"
+        ]
+      },
+      {
+        id: "bombon",
+        name: "Palito bombón americana x40",
+        variants: null
+      },
+      {
+        id: "cono",
+        name: "Cono bola",
+        variants: [
+          "Americana",
+          "Americana crocante",
+          "Chocolate",
+          "Dulce de leche"
+        ]
+      },
+      {
+        id: "trescientos",
+        name: "360",
+        variants: [
+          "Americana, vainilla y dulce de leche",
+          "Chocolate, frutilla y vainilla"
+        ]
+      }
+    ]
+  }
+]
